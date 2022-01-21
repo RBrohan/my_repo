ffc8484a31b5f8dfe1c069c3d5a8743331bc1c34
RBrohan <rohan182b@gmail.com>
2022-01-21 17:20:33 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { ChatEngine } from "react-chat-engine";
+import "./App.css";
+
+const App = () => {
+  return (
+    <ChatEngine
+      height="100vh"
+      projectID="6521e5a6-e950-482c-81e9-4a8aea2f193a"
+      userName=""
+      userSecret="e683b61d-b3ff-411f-80e5-7344cd993c68"
+    />
+  );
+};
+export default App;
